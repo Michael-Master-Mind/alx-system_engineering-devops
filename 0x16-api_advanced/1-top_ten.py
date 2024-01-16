3338af8334cadb48d0b95682ce11e6f3d959c761
Michael Demissew AB <michaeldemissew@yahoo.com>
2024-01-16 23:28:50 +0300

    Advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
-'''
-module for the Reddit API
-'''
-
+"""
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
+"""
 import requests
+import sys
 
 
-def top_ten(subscribers):
-    '''function that queries and returns the top 10 subscribers '''
-    headers = {'User-agent': 'test'}
-    res = requests.get("https://www.reddit.com/r/{}/hot.json"
-                       .format(subscribers), headers=headers)
-    try:
-        data = res.json()['data']['children']
-    except Exception:
+def top_ten(subreddit):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'limit': 10
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+    if res.status_code != 200:
         print(None)
         return
-    for i in range(0, 10):
-        print(data[i]['data']['title'])
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    if len(hot_posts) is 0:
+        print(None)
+    else:
+        for post in hot_posts:
+            print(post['data']['title'])
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python3
-'''
-module for the Reddit API
-'''
-
+"""
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
+"""
 import requests
+import sys
 
 
-def recurse(subreddit, hot_list=[], nxt=None):
-    '''function that recursively queries and returns the top 10 subscribers '''
-    headers = {'User-agent': 'test'}
-    res = requests.get("https://www.reddit.com/r/{}/hot.json?after={}"
-                       .format(subreddit, nxt), headers=headers)
-    try:
-        data = res.json()['data']
-    except Exception:
+def add_title(hot_list, hot_posts):
+    """ Adds item into a list """
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'after': after
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+
+    if res.status_code != 200:
         return None
-    nxt = data['after']
-    posts = data['children']
-    for post in posts:
-        hot_list.append(post['data']['title'])
-    if nxt:
-        recurse(subreddit, hot_list, nxt)
-    return hot_list
+
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    add_title(hot_list, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
